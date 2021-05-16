99a2cd3ebd3ff6b9a0f536302356be8157402c97
renatogcruz <renatogcruz@hotmail.com>
2021-05-16 14:59:07 -0300

    explorar - cap. 1

--- data_science/r_para_data_science/Cap. 01/Explorar.R
+++ data_science/r_para_data_science/Cap. 01/Explorar.R
@@ -39,5 +39,58 @@ ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy), shape = 8) # de 0 até 20
 
 
+# FACETAS - página 14
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_wrap(~class, nrow = 2)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_grid(drv ~ cyl)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_grid(. ~ cyl)
+
+# OBJETOS GEOMÉTRICOS - página 16
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) # geom de pontos
+
+ggplot(data = mpg) + 
+  geom_smooth(mapping = aes(x = displ, y = hwy)) # geom de smooth
+  
+ggplot(data = mpg) + 
+  geom_smooth(mapping = aes(x = displ, y = hwy, linetype = drv))
 
 
+ggplot(data = mpg) + 
+  geom_smooth(mapping = aes(x = displ, y = hwy, group = drv))
+
+ggplot(data = mpg) + 
+  geom_smooth(
+    mapping = aes(x = displ, y = hwy, linetype = drv),
+    show.legend = FALSE
+    )
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point() + 
+  geom_smooth()
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point(mapping = aes(color = class)) + 
+  geom_smooth()
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point(mapping = aes(color = class)) + 
+  geom_smooth(
+    data = filter(mpg, class == "subcompact"),
+    se = FALSE
+  )
+
+# TRANSFORMAÇÕES ESTATÍSTICAS - página 22
